291e29b29290d91bcfed26f3a1d89c7453addeff
sutanu5 <sutanu.sain@gmail.com>
2021-07-01 18:36:09 +0530

    init push

---
+++ build/precache-manifest.2f154afb9ef65259511098177b9dd117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a7c7311085a6194804815dfde6d3a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58d1db557316afe93bfc",
+    "url": "/static/css/main.b548183b.chunk.css"
+  },
+  {
+    "revision": "8fbd2dfa219863d68ea2",
+    "url": "/static/js/2.81e5a61b.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/static/js/2.81e5a61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d1db557316afe93bfc",
+    "url": "/static/js/main.3e77dbea.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
